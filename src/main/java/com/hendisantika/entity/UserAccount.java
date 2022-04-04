0a04bc6502d0efc91948a98349d965b5f3fe0703
Hendi Santika <hendisantika@gmail.com>
2022-04-04 07:39:33 +0700

    #9 - Add UserAccount entity

---
+++ src/main/java/com/hendisantika/entity/UserAccount.java
@@ -0,0 +1,35 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-rbac
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/22
+ * Time: 07.39
+ */
+@Entity
+@Data
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean active;
+    @OneToMany(mappedBy = "user")
+    private List<UserToRole> userToRoles;
+}
