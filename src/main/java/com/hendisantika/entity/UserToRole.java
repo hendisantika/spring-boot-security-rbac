f1a9cf39a535abcde4402c26438dc88f476be0ec
Hendi Santika <hendisantika@gmail.com>
2022-04-04 07:37:40 +0700

    #5 - Add UserToRole entity

---
+++ src/main/java/com/hendisantika/entity/UserToRole.java
@@ -0,0 +1,30 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-rbac
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/22
+ * Time: 07.37
+ */
+@Data
+@Entity
+public class UserToRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    private UserAccount user;
+    @ManyToOne
+    private UserRole role;
+}
