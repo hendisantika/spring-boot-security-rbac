17cf4eefd2504dc3aafbbb6b9b6f990d371edc5d
Hendi Santika <hendisantika@gmail.com>
2022-04-04 07:40:33 +0700

    #11 - Add UserPrivilegeRepository

---
+++ src/main/java/com/hendisantika/repository/UserPrivilegeRepository.java
@@ -0,0 +1,19 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-rbac
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/22
+ * Time: 07.40
+ */
+@Repository
+public interface UserPrivilegeRepository extends JpaRepository<UserAccount, Integer> {
+    UserAccount findByUsername(String username);
+}
