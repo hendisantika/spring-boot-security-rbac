1dfabc4f3d71251b9c9c4efc921fd073796305e3
Hendi Santika <hendisantika@gmail.com>
2022-04-04 07:41:11 +0700

    #12 - Add CustomUserDetails

---
+++ src/main/java/com/hendisantika/config/CustomUserDetails.java
@@ -0,0 +1,67 @@
+package com.hendisantika.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-rbac
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/22
+ * Time: 07.40
+ */
+public class CustomUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final boolean active;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username, String password, boolean active,
+                             Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
